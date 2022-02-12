cb92f6ef2ffb67b351557b0453a0bc642705fbee
fleisan18 <92662450+fleisan18@users.noreply.github.com>
2022-02-12 13:27:29 +0500

    float type added

--- logicalc.py
+++ logicalc.py
@@ -1,10 +1,20 @@
 from tkinter import *
 import ui_calc
 
-def logic_calc(self, operation):
-        pass
-    
-def update():
-       pass
+def logicalc(operation):
+    if operation == "C":
+        formula = ""
+    elif operation == "DEL":
+        formula = formula[0:-1]
+    elif operation == "X^2":
+       formula = str((eval(formula))**2)
+    elif operation == "=":
+        formula = str(eval(formula))
+    elif operation == ".":
+        formula = str(float(formula))
+    else:
+        if formula == "0":
+            formula = ""
+        formula += operation
 
 
