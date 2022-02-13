a0cb01e9547f5791c8747ef5261748d99e48f06e
MrFedko <miklerst@mail.ru>
2022-02-13 10:05:26 +0300

    реализован перевод в двоичную систему

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()      
 window.title("Калькулятор выходного дня")
 window["bg"] = "#000"
-window.geometry("485x550+200+200")
+window.geometry("400x350+500+250")
 window.resizable(False, False)
 formula = "0"
 lbl = Label(text=formula, font=("Times New Roman", 21, "bold"), bg="#000", foreground="#FFF")
@@ -12,15 +12,20 @@ btns = ["C", "(", ")", "*",
         "1", "2", "3", "/",
         "4", "5", "6", "+",
         "7", "8", "9", "-",
-        "DEL", "0", ".", "="
+        "0101", "0", ".", "="
         ]     
 
 def commands(any_char):
     global formula 
     if any_char == "C":
         formula = ""
-    elif any_char == "DEL":
-        formula = formula[0:-1]
+    elif any_char == "0101":
+        formula = int(formula)
+        result = ''
+        while formula != 0:
+            result = str(formula % 2) + result
+            formula //= 2
+        formula = result
     elif any_char == "=":
         formula = str(eval(formula))
     else:
@@ -36,14 +41,14 @@ def update():
         formula = "0"
     lbl.configure(text=formula)
 
-x = 10
-y = 140
+x = 0
+y = 100
 for bt in btns:
     com = lambda x=bt: commands(x)
-    Button(text=bt, bg="#FFF", font=("Times New Roman", 15), command=com).place(x=x, y=y, width=115, height=79)
-    x += 117
-    if x > 400:
-        x = 10
-        y += 81
+    Button(text=bt, bg="#FFF", font=("Times New Roman", 15), command=com).place(x=x, y=y, width=100, height=50)
+    x += 100
+    if x > 300:
+        x = 0
+        y += 50
 
 window.mainloop()
